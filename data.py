4834cc1f6c6edc3e1053d1d11f0430d97ac16953
LINK228-ux <Kuala.203@mail.ru>
2024-06-19 15:53:51 +0300

    data.py

--- data.py
+++ data.py
@@ -6,16 +6,16 @@ headers = {
 # содержат имя, фамилию, адрес заказчика, ближайшую к заказчику станцию метро, телефон заказчика,
 # количество дней аренды,дату доставки,комментарий от заказчика, предпочитаемые цвета
 order_body = {
-    "firstName": "Kirill",
-    "lastName": "Pokrovskiy",
-    "address": "Dachnaya, 9",
+    "firstName": "Oleg",
+    "lastName": "Mishkin",
+    "address": "Leninna, 6",
     "metroStation": 6,
-    "phone": "+7 800 555 35 35",
+    "phone": "+7 912 15 32 193",
     "rentTime": 1,
-    "deliveryDate": "2024-10-10",
+    "deliveryDate": "2024-06-19",
     "comment": "None",
     "color": [
-        "BLACK"
+        "WHITE"
     ]
 }
 
